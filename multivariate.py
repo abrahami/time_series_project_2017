b46ee13b1b42f32f105fca884a61e12de0464bfc
abrahami <abraham.israeli@intel.com>
2018-01-10 21:22:05 +0200

    Adding an evaluation function and adding its usage along the code

--- multivariate.py
+++ multivariate.py
@@ -11,10 +11,11 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import *
 from keras.optimizers import Nadam
 from keras.initializers import *
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
 import sax_word as pysax
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+
 
 # general functions created in Rachnog's git
 def shuffle_in_unison(a, b):
@@ -27,6 +28,37 @@ def shuffle_in_unison(a, b):
         shuffled_a[new_index] = a[old_index]
         shuffled_b[new_index] = b[old_index]
     return shuffled_a, shuffled_b
+
+
+def evaluate_model(true_values, predicted_values, verbose = True):
+    '''
+    Evaluation process of any classification model . Yields the accuracy measure and the F1.
+    Useful mainly for multi label problems. Note that length of the
+    :param true_values: list (of probably integers)
+        list of the true values
+    :param predicted_values: list (of probably integers)
+        list of the predicted values. Not probability but rather the final decision of the algorithm
+    :param verbose: boolean
+        Whether or not to print the results to the screen at the end of the run
+    :return: dictionary with two values - the accuracy and the F1 (weighted F1)
+
+    Example
+    -------
+    >>>from sklearn.metrics import confusion_matrix, f1_score
+    >>>import numpy as np
+    >>>y_true = [1,2,3,1,2,3,1,2,3,1]
+    >>>y_pred = [1,2,3,1,2,3,2,2,2,2]
+    >>>res = evaluate_model(true_values=y_true, predicted_values=y_pred)
+    '''
+    if len(true_values) != len(predicted_values):
+        print("Length of the two vectors is not equal. Please fix and run again")
+    conf_matrix = confusion_matrix(y_true=true_values, y_pred=predicted_values)
+    acc_measure = np.trace(conf_matrix) * 1.0 / conf_matrix.sum() * 1.0
+    f1_measure = f1_score(y_true=true_values, y_pred=predicted_values, average='weighted')
+    if verbose:
+        print("\nCurrent run of the evaluation function yielded the following results:\n"
+              "Accuracy measure: {}; F1 measure: {}".format(acc_measure, f1_measure))
+    return {'acc_measure': round(acc_measure, 3), 'f1_measure': round(f1_measure, 3)}
 
 
 def create_Xt_Yt(X, y, percentage=0.7):
@@ -55,6 +87,7 @@ def readucr(filename):
     Y = data[:, 0]
     X = data[:, 1:]
     return X, Y
+
 
 def sax_data_prep(x_train, x_test, sax_obj=pysax.SAXModel(window=20, stride=5, nbins=5, alphabet="ABCD")):
     tfidf_obj = TfidfVectorizer(norm='l2', min_df=0, use_idf=True, smooth_idf=False, sublinear_tf=True)
@@ -141,11 +174,13 @@ def run_algo(dataset, ephocs=100, with_sax=False):
             nb_classes = len(np.unique(y_test))
 
             # under-sampling, only for debugging purposes!!!!!
+            '''
             x_train = x_train[0:300]
             y_train = y_train[0:300]
             x_test = x_test[0:200]
             y_test = y_test[0:200]
             nb_classes = len(np.unique(y_test))
+            '''
 
             # normalization
             y_train = (y_train - y_train.min()) / (y_train.max() - y_train.min()) * (nb_classes - 1)
@@ -223,20 +258,18 @@ def run_algo(dataset, ephocs=100, with_sax=False):
     train_proba_prediction = model.predict(x=x_train_array, batch_size=128, verbose=1)
     train_prediction = np.apply_along_axis(func1d=np.argmax, axis=1, arr=train_proba_prediction)
     y_train_flatten = np.array([idx for i in y_train_array for idx, j in enumerate(list(i)) if j])
-    conf_matrix = confusion_matrix(y_true=y_train_flatten.astype(int), y_pred=train_prediction.astype(int))
-    error_rate = 1 - np.trace(conf_matrix) * 1.0 / conf_matrix.sum() * 1.0
-    print("\nError rate of current run over train data is {}".format(error_rate))
+    train_res = evaluate_model(true_values=y_train_flatten, predicted_values=train_prediction, verbose=False)
+    print("\nError rate of current run over train data is {}".format(train_res))
 
     test_proba_prediction = model.predict(x=x_test_array, batch_size=128, verbose=1)
     test_prediction = np.apply_along_axis(func1d=np.argmax, axis=1, arr=test_proba_prediction)
     y_test_flatten = np.array([idx for i in y_test_array for idx, j in enumerate(list(i)) if j])
-    conf_matrix = confusion_matrix(y_true=y_test_flatten.astype(int), y_pred=test_prediction.astype(int))
-    error_rate = 1 - np.trace(conf_matrix) * 1.0 / conf_matrix.sum() * 1.0
-    print("\nError rate of current run over test data is {}".format(error_rate))
+    test_res = evaluate_model(true_values=y_test_flatten, predicted_values=test_prediction, verbose=False)
+    print("\nError rate of current run over test data is {}".format(test_res))
 
 if __name__ == '__main__':
     # should be one out of the two
     dataset = "WaveGesture"#"ECG"
-    ephocs = 1000
+    ephocs = 10
     data_location = "C:\\Users\\abrahami\\Documents\\Private\\Uni\\BGU\\time_series\\project\\data"
-    run_algo(dataset=dataset, ephocs=ephocs, with_sax=True)
+    run_algo(dataset=dataset, ephocs=ephocs, with_sax=False)
